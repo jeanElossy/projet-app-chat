572dbdec699ece18f08928198583fe0519c81599
jean elossy <jean.elossy@yahoo.com>
2021-08-21 08:09:11 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
 const sharingIcons = document.querySelector(".sharing__icons");
 
 const display = () => {
-  document.querySelector(".collapse").classList.toggle("toggle");
   setTimeout(() => { 
-      document.querySelector(".icon--user").classList.toggle("toggle");
-  }, 250);
+    document.querySelector(".icon--user").classList.toggle("toggle");
+  }, 300);
 
   setTimeout(() => { 
     document.querySelector(".icon--file").classList.toggle("toggle");
+  }, 250);
+
+  setTimeout(() => { 
+    document.querySelector(".icon--camera").classList.toggle("toggle");
   }, 200);
 
   setTimeout(() => { 
-      document.querySelector(".icon--camera").classList.toggle("toggle");
+    document.querySelector(".icon--image").classList.toggle("toggle");
   }, 150);
-
-  setTimeout(() => { 
-      document.querySelector(".icon--image").classList.toggle("toggle");
-  }, 100);
 };
 sharingIcons.addEventListener("click", () => display());
 
