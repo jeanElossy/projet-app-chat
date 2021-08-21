6c18f0d02c895c7f91edbb3a909b0c8e835a07f2
jean elossy <jean.elossy@yahoo.com>
2021-08-21 07:42:09 +0200

    another update file js and css

--- js/script.js
+++ js/script.js
@@ -2,6 +2,21 @@ const sharingIcons = document.querySelector(".sharing__icons");
 
 const display = () => {
   document.querySelector(".collapse").classList.toggle("toggle");
+  setTimeout(() => { 
+      document.querySelector(".icon--user").classList.toggle("toggle");
+  }, 250);
+
+  setTimeout(() => { 
+    document.querySelector(".icon--file").classList.toggle("toggle");
+  }, 200);
+
+  setTimeout(() => { 
+      document.querySelector(".icon--camera").classList.toggle("toggle");
+  }, 150);
+
+  setTimeout(() => { 
+      document.querySelector(".icon--image").classList.toggle("toggle");
+  }, 100);
 };
 sharingIcons.addEventListener("click", () => display());
 
